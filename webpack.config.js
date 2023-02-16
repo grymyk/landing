a3081ad689e412c6d2d22075ed2ad5dfb433fc2f
Yurii <yurchikvtope@gmail.com>
2023-02-16 19:28:49 +0000

    all

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
  module.exports = {
    mode: 'development',
    entry: {
-     index: './src/index.js',
+     index: './src/components/nav/index.js',
    },
    devtool: 'inline-source-map',
    devServer: {
